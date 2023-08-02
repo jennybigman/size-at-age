a890dcfc967642b19b53014639f1e703ff0bd925
jennybigman <jennifersbigman@gmail.com>
2023-08-01 19:14:15 -0700

    added oxygen data

--- scripts/working on streamlining data wrangling and adding temp metrics.R
+++ scripts/working on streamlining data wrangling and adding temp metrics.R
@@ -1,4 +1,21 @@
-
+	# libraries
+	library(here)
+	library(tidyverse)
+	library(lubridate)
+	library(mgcv)
+	library(MuMIn)
+	library(visreg)
+	library(data.table)
+	library(gamm4)
+	library(tidymv)
+	library(brms)
+	library(mgcViz)
+	library(patchwork)
+	library(grid)
+	library(gratia)
+	library(sf)
+	library(AICcmodavg)
+	
 	#### read in specimen age & weight data ####
 	
 	specimen_dat <- read.csv(file = here("./data/df_list_wrangled_names.csv"))
@@ -7,7 +24,7 @@
 	spec_dat_0 <- specimen_dat %>%
 		dplyr::select(species, age) %>%
 		group_by(species) %>%
-		summarise(NAs = sum(is.na(age)))
+		summarise(NAs = sum(is.na(age))) 
 	
 	# join specimen data and ROMS temp
 	
@@ -39,88 +56,114 @@
 		mutate(ID_f = as.factor(ID),
 					 haul_id_f = as.factor(haul_id))
 		
+		
 	}
 	
 	specimen_dat_list <- lapply(specimen_dat_list, ranef_setup_func)
+	
+	# add columns
+	
+	weight_metrics_func <- function(x){ 
+
+		# calculate mean and sd weight by age
+		dat <- x %>%
+		group_by(age_f) %>%
+		mutate(mean_wt_age = mean(log_wt),
+					 sd_wt_age = sd(log_wt)) %>%
+		rowwise() %>%
+		mutate(log_wt_scaled = (log_wt - mean_wt_age)/sd_wt_age)
+	
+	}
+	
+	specimen_dat_list <- lapply(specimen_dat_list, weight_metrics_func)
 	
 	#### temp output ####
 	
 	# load ACLIM temps
 	
 	# bias corrected using delta method
-  load("../../ACLIM2/Data/out/K20P19_CMIP6/allEBS_means/ACLIM_weekly_hind_mn.Rdata")
-  load("../../ACLIM2/Data/out/K20P19_CMIP6/allEBS_means/ACLIM_weekly_fut_mn.Rdata")
-
-  # newest ACLIM indices but 3 basins (NEBS, SEBS, other?)
-	#load("../../ACLIM2/Data/out/Feb 13/K20P19_CMIP6/allEBS_means/ACLIM_weekly_hind_mn.Rdata")
-  #load("../../ACLIM2/Data/out/Feb 13/K20P19_CMIP6/allEBS_means/ACLIM_weekly_fut_mn.Rdata")
+  load("../../ACLIM2/Data/out/Mar 2023/K20P19_CMIP6-001/allEBS_means/ACLIM_weekly_hind_mn.Rdata")
+  load("../../ACLIM2/Data/out/Mar 2023/K20P19_CMIP6-001/allEBS_means/ACLIM_weekly_fut_mn.Rdata")
 
   # for PC
-  load("/data/ACLIM_weekly_hind_mn.Rdata")
-  load("/data/ACLIM_weekly_fut_mn.Rdata")
+  #load("/data/ACLIM_weekly_hind_mn.Rdata")
+  #load("/data/ACLIM_weekly_fut_mn.Rdata")
 
-  # filter out bottom temps and stick to SEBS
-  temp_hind <- ACLIM_weekly_hind %>%
-    filter(var == "temp_bottom5m") %>%
+  # filter out bottom temp & oxygen and stick to SEBS
+  
+  vars <- c("temp_bottom5m", "oxygen_bottom5m")
+  
+  hind_var <- ACLIM_weekly_hind %>%
+    filter(var %in% vars) %>%
   	filter(basin == "SEBS")
   
-  temp_proj <- ACLIM_weekly_fut %>%
-    filter(var == "temp_bottom5m") %>%
+  proj_var <- ACLIM_weekly_fut %>%
+    filter(var %in% vars) %>%
   	filter(year >= 2021) %>%
   	filter(basin == "SEBS")
-
+  
 	#### 1. Spring/Summer temps preceding survey ####
 	
   presurvey <- 4:6 # (April to June)
   
-  presurvey_hind <- temp_hind %>%
+  presurvey_hind_var <- hind_var %>%
   	filter(mo %in% presurvey) %>%
-  	group_by(year) %>%
-  	summarise(presurvey_mean_temp= mean(mn_val))
+  	group_by(year, var) %>%
+  	summarise(presurvey_mean_val = mean(mn_val))
   
+  presurvey_hind_var_short <- 
+  	spread(presurvey_hind_var, key = var, value = presurvey_mean_val) %>%
+  	rename(presurvey_btemp = "temp_bottom5m",
+  				 presurvey_boxy = "oxygen_bottom5m")
+  	
+  					
   # add to dataframe list
-	temp_join_func <- function(x){
+	dat_join_func <- function(x){
  
- 			specimen_dat <- left_join(x, presurvey_hind, by = "year")
+ 			specimen_dat <- left_join(x, presurvey_hind_var_short, by = "year")
 	}
  
-	specimen_temp_dat <- lapply(specimen_dat_list, temp_join_func)
+	specimen_dat <- lapply(specimen_dat_list, dat_join_func)
 
 
   #### 2. Yearly temp -- avg of temp July - Dec of year before survey & Jan - June year of survey) ####
   
-  temp_yr_prior_func <- function(x){
+  yr_prior_func <- function(x){
    	
    	# temp same year as collected, Jan - June
-   	temp_current <- temp_hind %>%
+   	current <- hind_var %>%
    		filter(year == x & mo <= 6)
  
    	# temp previous year July - Dec
-		temp_previous <- temp_hind %>%
+		previous <- hind_var %>%
   		filter(year == x - 1 & mo > 6)
  
 		# combine and take a mean temp July - June
-  	temp_yr <- bind_rows(temp_current, temp_previous) %>%
-  		summarise(mean_yr_temp = mean(mn_val)) %>%
+  	var_yr <- bind_rows(current, previous) %>%
+  		group_by(var) %>%
+  		summarise(mean_yr = mean(mn_val)) %>%
   		mutate(year = x)
    
    }
    
-  temp_yr_prior <- lapply(1971:2020, temp_yr_prior_func) %>% bind_rows()
+  yr_prior <- lapply(1971:2020, yr_prior_func) %>% bind_rows()
   
-  temp_join_func <- function(x){
+  yr_prior_short <- 
+  	spread(yr_prior, key = var, value = mean_yr) %>%
+		rename(yrprior_btemp = "temp_bottom5m",
+  				 yrprior_boxy = "oxygen_bottom5m")
+  	  
+  
+  dat_join_func <- function(x){
  
- 			specimen_dat <- left_join(x, temp_yr_prior, by = "year")
+ 			specimen_dat <- left_join(x, yr_prior_short, by = "year")
 	}
  
-	specimen_temp_dat <- lapply(specimen_temp_dat, temp_join_func)
+	specimen_dat <- lapply(specimen_dat, dat_join_func)
 
-  df_test <- specimen_temp_dat[[1]]
-  
   #### 3. temp during first year of life ####
   
-
-	temp_age0_func <- function(x){
+ age0_func <- function(x){
 		
 		df <- x %>%
 		mutate(year_age0 = cohort,
@@ -131,16 +174,17 @@
 			rename(year = year_age0) %>%
 			distinct()
 		
-		yr_age0_temps <- left_join(yr_age0_dat, temp_yr_prior) %>%
-			rename(temp_age0 = mean_yr_temp) %>%
+		yr_age0_vars <- left_join(yr_age0_dat, yr_prior_short) %>%
+			rename(age0_btemp = yrprior_btemp,
+						 age0_boxy = yrprior_boxy) %>%
 			dplyr::select(-year)
 	
-		dat <- left_join(df, yr_age0_temps)
+		dat <- left_join(df, yr_age0_vars)
 	
 
 	}	
 	
-	specimen_temp_dat <- lapply(specimen_temp_dat, temp_age0_func)
+	specimen_dat <- lapply(specimen_dat, age0_func)
 	
 	# change jday to julian_day to match model parameterization
 	
@@ -148,11 +192,46 @@
 		df <- x %>% rename(julian_day = jday)
 	}
 	
-	specimen_temp_dat <- lapply(specimen_temp_dat, rename_func)
+	specimen_dat <- lapply(specimen_dat, rename_func)
 	
-	pollock_dat <- specimen_temp_dat[[1]]
-	pcod_dat <- specimen_temp_dat[[2]]
-	yfinsole_dat <- specimen_temp_dat[[3]]
+	names(specimen_dat) <- c("pcod_dat", "pollock_dat", "yfinsole_dat")
+	
+	# separate for species-specific wrangling tasks
+	pcod_dat <- specimen_dat[[1]]
+	pollock_dat <- specimen_dat[[2]]
+	yfinsole_dat <- specimen_dat[[3]]
+	
+	pcod_age_sum <- pcod_dat %>%
+		group_by(age_f) %>%
+		summarize(n())
+		
+		pcod_age_sum %>%
+			head()
+	
+	pollock_age_sum <- pollock_dat %>%
+		group_by(age_f) %>%
+		summarize(n()) 
+	
+		pollock_age_sum	%>%
+			head()
+		
+	yfinsole_age_sum <- yfinsole_dat %>%
+		group_by(age_f) %>%
+		summarize(n()) 
+	
+		yfinsole_age_sum %>%
+			head()
+	
 
+	# trim data set by largest age with >= 100 samples
+	pcod_dat <- pcod_dat  %>% filter(between(age, 1, 28))
+
+	pollock_dat <- pollock_dat  %>% filter(between(age, 1, 10))
+
+	yfinsole_dat <- yfinsole_dat  %>% filter(between(age, 1, 20))
+
+
+	
+	
   
  
