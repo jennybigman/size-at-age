1d43b317219eba85555f2a94f0517693f620cd7f
jennybigman <jennifersbigman@gmail.com>
2023-08-01 19:13:50 -0700

    added oxygen models

--- scripts/02c - gams fitted with ML.R
+++ scripts/02c - gams fitted with ML.R
@@ -1,6 +1,10 @@
-# gams with smoothed age and temp effects 
+# gams with smoothed age and temp/oxygen effects 
 
-	#### presurvey temp ####
+	##############################################################################
+	# temperature ####
+	##############################################################################
+	
+	## presurvey temp ####
 	
 	# pollock #	
   
@@ -313,3 +317,139 @@
 
 	saveRDS(yfin_temp0_int_age_bam_ML,
 					file = here("./output/model output PC/yfin_temp0_int_age_bam_ML.rds"))
+
+	
+	##############################################################################
+	# Oxygen ####
+	##############################################################################
+
+	## presurvey oxygen (oxygen April to June of same year as survey) ####
+
+	#1. weight ~ age + oxygen
+	
+	presurvey_boxy_func <- function(df){
+	
+	bam(log_wt ~ age_f + s(presurvey_boxy) + 
+														 	s(julian_day) + te(latitude, longitude) + 
+											  			s(ID_f, bs = "re") + # haul in year random effect
+								 			  			s(haul_id_f, bs = "re") + # haul in year random effect
+								 			  			s(cohort_f, bs = "re"),
+											  			data = df, 
+															nthreads = 8,
+															method = "ML")
+	}
+	
+	presurvey_boxy_fit <- lapply(specimen_dat, presurvey_boxy_func)
+	
+	#### EDIT ####
+  #saveRDS(pol_pretemp_age_bam, 
+  #				file = here("./output/model output/ACLIM temps/pol_pretemp_age_bam.rds"))
+  #### EDIT ####
+ 
+  # 2. weight ~ age * oxygen
+	
+  presurvey_boxy_int_func <- function(df){
+
+  	bam(log_wt ~ age_f + s(presurvey_boxy, by = age_f) + 
+																 	s(julian_day) + te(latitude, longitude) + 
+																	s(ID_f, bs = "re") + # haul in year random effect
+								 									s(haul_id_f, bs = "re") + # haul in year random effect
+								 									s(cohort_f, bs = "re"),
+																	data = df, 
+																	nthreads = 8,
+																	method = "ML")
+  }
+	
+	presurvey_boxy_int_fit <- lapply(specimen_dat, presurvey_boxy_int_func)
+
+  #### EDIT ####													
+	#saveRDS(pol_pretemp_int_age_bam_ML, 
+		# 				file = here("./output/model output PC/pol_pretemp_int_age_bam_ML.rds"))
+	#### EDIT ####
+	
+	#### year prior oxygen average ####
+	
+	#1. weight ~ age + oxygen
+	
+	yrprior_boxy_func <- function(df){
+
+		bam(log_wt ~ age_f + s(yrprior_boxy) + 
+															s(julian_day) + te(latitude, longitude) + 
+															s(ID_f, bs = "re") + # haul in year random effect
+								 							s(haul_id_f, bs = "re") + # haul in year random effect
+								 							s(cohort_f, bs = "re"),
+															data = df, 
+															nthreads = 8,
+															method = "ML")
+	}
+	
+	yrprior_boxy_fit <- lapply(specimen_dat, yrprior_boxy_func)
+
+	  ##### EDIT ####													
+		# saveRDS(pol_yrtemp_age_bam_ML, 
+		#		file = here("./output/model output PC/pol_pretemp_age_bam_ML.rds"))
+	  ##### EDIT ####													
+
+	# 2. weight ~ age * oxygen 
+	
+	yrprior_boxy_int_func <- function(df){
+
+			bam(log_wt ~ age_f + s(yrprior_boxy, by = age_f) + 
+																	s(julian_day) + te(latitude, longitude) + 
+																	s(ID_f, bs = "re") + # haul in year random effect
+								 									s(haul_id_f, bs = "re") + # haul in year random effect
+								 									s(cohort_f, bs = "re"),
+																	data = df, 
+																	nthreads = 8,
+																	method = "ML")
+		}
+	
+		yrprior_boxy_int_fit <- lapply(specimen_dat, yrprior_boxy_int_func)
+	
+	#### EDIT ####													
+	#saveRDS(pol_yrtemp_int_age_bam, 
+  #				file = here("./output/model output/ACLIM temps/pol_yrtemp_int_age_bam.rds"))
+  #### EDIT ####													
+
+  #### oxygen during first year of life ####
+	
+	#1. weight ~ age + oxygen
+	
+	firstyr_boxy_func <- function(df){
+	
+			bam(log_wt ~ age_f + s(age0_boxy) + 
+													 	s(julian_day) + te(latitude, longitude) + 
+														s(ID_f, bs = "re") + # haul in year random effect
+								 						s(haul_id_f, bs = "re") + # haul in year random effect
+								 						s(cohort_f, bs = "re"),
+														data = pollock_dat, 
+														nthreads = 8,
+														method = "ML")
+	}
+		
+	firstyr_boxy_fit <- lapply(specimen_dat, firstyr_boxy_func)
+	
+	#### EDIT ####													
+  #saveRDS(pol_temp0_age_bam, file = here("./output/model output/ACLIM temps/pol_temp0_age_bam.rds"))
+ 	#### EDIT ####													
+
+
+  # 2. weight ~ age * oxygen
+	
+	firstyr_boxy_int_func <- function(df){
+		
+			bam(log_wt ~ age_f + s(age0_boxy, by = age_f) + 
+															 	s(julian_day) + te(latitude, longitude) + 
+																s(ID_f, bs = "re") + # haul in year random effect
+								 								s(haul_id_f, bs = "re") + # haul in year random effect
+								 								s(cohort_f, bs = "re"),
+																data = pollock_dat, 
+																nthreads = 8,
+																method = "ML")	
+	}
+		
+	firstyr_boxy_int_fit <- lapply(specimen_dat, firstyr_boxy_int_func)
+
+	#### EDIT ####													
+  #saveRDS(pol_temp0_age_bam, file = here("./output/model output/ACLIM temps/pol_temp0_age_bam.rds"))
+ 	#### EDIT ####													
