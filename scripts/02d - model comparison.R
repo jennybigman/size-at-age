b1e42e6e92539cf42c69cf97018e43f4e1f64947
jennybigman <jennifersbigman@gmail.com>
2023-08-16 11:01:20 -0700

    added oxy models

--- scripts/02d - model comparison.R
+++ scripts/02d - model comparison.R
@@ -9,110 +9,185 @@
   
   # pollock
   
-   pol_temp_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/pol_temp_age_bam_ACLIM_SEBS_ML.rds"))
-   pol_temp_int_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/pol_temp_int_age_bam_ACLIM_SEBS_ML.rds"))
+   pol_pretemp_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/pol_pretemp_age_bam_ML.rds"))
+   pol_pretemp_int_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/pol_pretemp_int_age_bam_ML.rds"))
 
-   AIC(pol_temp_age_bam_ACLIM_SEBS_ML)
-   AIC(pol_temp_int_age_bam_ACLIM_SEBS_ML)
+   AIC(pol_pretemp_age_bam_ML)
+   AIC(pol_pretemp_int_age_bam_ML)
    
    # pcod
   
-   pcod_temp_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/pcod_temp_age_bam_ACLIM_SEBS_ML.rds"))
-   pcod_temp_int_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/pcod_temp_int_age_bam_ACLIM_SEBS_ML.rds"))
+   pcod_pretemp_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/pcod_pretemp_age_bam_ML.rds"))
+   pcod_pretemp_int_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/pcod_pretemp_int_age_bam_ML.rds"))
 
-   AIC(pcod_temp_age_bam_ACLIM_SEBS_ML)
-   AIC(pcod_temp_int_age_bam_ACLIM_SEBS_ML)
+   AIC(pcod_pretemp_age_bam_ML)
+   AIC(pcod_pretemp_int_age_bam_ML)
    
    # yfin sole
    
-   yfin_temp_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/yfin_temp_age_bam_ACLIM_SEBS_ML.rds"))
-   yfin_temp_int_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/yfin_temp_int_age_bam_ACLIM_SEBS_ML.rds"))
+   yfin_pretemp_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/yfin_pretemp_age_bam_ML.rds"))
+   yfin_pretemp_int_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/yfin_pretemp_int_age_bam_ML.rds"))
 
-   AIC(yfin_temp_age_bam_ACLIM_SEBS_ML)
-   AIC(yfin_temp_int_age_bam_ACLIM_SEBS_ML)
+   AIC(yfin_pretemp_age_bam_ML)
+   AIC(yfin_pretemp_int_age_bam_ML)
  
    
-   #### temps avg for all months annually ACLIM ####
+   #### year temp the year preceding survey ####
 	
 	# pollock
    
-  pol_yrtemp_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/pol_yrtemp_age_bam_ACLIM_SEBS_ML.rds"))
-  pol_yrtemp_int_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/pol_yrtemp_int_age_bam_ACLIM_SEBS_ML.rds"))
+  pol_yrtemp_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/pol_yrtemp_age_bam_ML.rds"))
+  pol_yrtemp_int_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/pol_yrtemp_int_age_bam_ML.rds"))
   
-  AIC(pol_yrtemp_age_bam_ACLIM_SEBS_ML)
-  AIC(pol_yrtemp_int_age_bam_ACLIM_SEBS_ML)
+  AIC(pol_yrtemp_age_bam_ML) ## need to run
+  AIC(pol_yrtemp_int_age_bam_ML)
   
 	# pcod #
 	
-	pcod_yrtemp_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/pcod_yrtemp_age_bam_ACLIM_SEBS_ML.rds"))
-  pcod_yrtemp_int_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/pcod_yrtemp_int_age_bam_ACLIM_SEBS_ML.rds"))
+	pcod_yrtemp_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/pcod_yrtemp_age_bam_ML.rds"))
+  pcod_yrtemp_int_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/pcod_yrtemp_int_age_bam_ML.rds"))
   
-  AIC(pcod_yrtemp_age_bam_ACLIM_SEBS_ML)
-  AIC(pcod_yrtemp_int_age_bam_ACLIM_SEBS_ML)
+  AIC(pcod_yrtemp_age_bam_ML)
+  AIC(pcod_yrtemp_int_age_bam_ML)
  
  # yellowfin # 
 													
-	yfin_yrtemp_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/yfin_yrtemp_age_bam_ACLIM_SEBS_ML.rds"))
-  yfin_yrtemp_int_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/yfin_yrtemp_int_age_bam_ACLIM_SEBS_ML.rds"))
+	yfin_yrtemp_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/yfin_yrtemp_age_bam_ML.rds"))
+  yfin_yrtemp_int_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/yfin_yrtemp_int_age_bam_ML.rds"))
   
-  AIC(yfin_yrtemp_age_bam_ACLIM_SEBS_ML)
-  AIC(yfin_yrtemp_int_age_bam_ACLIM_SEBS_ML)
+  AIC(yfin_yrtemp_age_bam_ML)
+  AIC(yfin_yrtemp_int_age_bam_ML)
 
-  # with REML
-  #yfin_yrtemp_age_bam_ACLIM_SEBS <- readRDS(file = here("./output/model output/ACLIM temps/yfin_yrtemp_age_bam_ACLIM_SEBS.rds"))
-  #yfin_yrtemp_int_age_bam_ACLIM_SEBS <- readRDS(file = here("./output/model output/ACLIM temps/yfin_yrtemp_int_age_bam_ACLIM_SEBS.rds"))
+  #### temp during first year of life #### EDIT BELOW
+	
+	# pollock
+   
+  pol_yrtemp_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/pol_yrtemp_age_bam_ML.rds"))
+  pol_yrtemp_int_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/pol_yrtemp_int_age_bam_ML.rds"))
   
-  #AIC(yfin_yrtemp_age_bam_ACLIM_SEBS)
-  #AIC(yfin_yrtemp_int_age_bam_ACLIM_SEBS)
-
+  AIC(pol_yrtemp_age_bam_ML) ## need to run
+  AIC(pol_yrtemp_int_age_bam_ML)
+  
+	# pcod #
+	
+	pcod_yrtemp_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/pcod_yrtemp_age_bam_ML.rds"))
+  pcod_yrtemp_int_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/pcod_yrtemp_int_age_bam_ML.rds"))
+  
+  AIC(pcod_yrtemp_age_bam_ML)
+  AIC(pcod_yrtemp_int_age_bam_ML)
+ 
+ # yellowfin # 
+													
+	yfin_yrtemp_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/yfin_yrtemp_age_bam_ML.rds"))
+  yfin_yrtemp_int_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/yfin_yrtemp_int_age_bam_ML.rds"))
+  
+  AIC(yfin_yrtemp_age_bam_ML)
+  AIC(yfin_yrtemp_int_age_bam_ML)
+  
+  
   #### compare different temp metrics ####
   
   # pollock 
   
-  pol_temp_int_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/pol_temp_int_age_bam_ACLIM_SEBS_ML.rds"))
-  pol_yrtemp_int_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/pol_yrtemp_int_age_bam_ACLIM_SEBS_ML.rds"))
-  pol_temp1_int_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/pol_temp1_int_age_bam_ACLIM_SEBS_ML.rds"))
-  pol_temp0_int_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/pol_temp0_int_age_bam_ACLIM_SEBS_ML.rds"))
+  pol_pretemp_int_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/pol_pretemp_int_age_bam_ML.rds"))
+  pol_yrtemp_int_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/pol_yrtemp_int_age_bam_ML.rds"))
+  pol_temp0_int_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/pol_temp0_int_age_bam_ML.rds"))
   # linear effect of temp and age
   pol_presurvey_templin_int_age_bam <- readRDS(file = here("./output/model output/ACLIM temps/linear models/pol_presurvey_templin_int_age_bam.rds"))
 
   
   # with temp by age effect -- summer avg temp more supported
-  AIC(pol_temp_int_age_bam_ACLIM_SEBS_ML)
-  AIC(pol_yrtemp_int_age_bam_ACLIM_SEBS_ML)
-  AIC(pol_temp1_int_age_bam_ACLIM_SEBS_ML)
-  AIC(pol_temp0_int_age_bam_ACLIM_SEBS_ML)
+  AIC(pol_pretemp_int_age_bam_ML)
+  AIC(pol_yrtemp_int_age_bam_ML)
+  AIC(pol_temp0_int_age_bam_ML)
   AIC(pol_presurvey_templin_int_age_bam)
 
-    # pcod 
+   # pcod 
   
-  pcod_temp_int_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/pcod_temp_int_age_bam_ACLIM_SEBS_ML.rds"))
-  pcod_yrtemp_int_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/pcod_yrtemp_int_age_bam_ACLIM_SEBS_ML.rds"))
-  pcod_temp1_int_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/pcod_temp1_int_age_bam_ACLIM_SEBS_ML.rds"))
-  pcod_temp0_int_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/pcod_temp0_int_age_bam_ACLIM_SEBS_ML.rds"))
+  pcod_pretemp_int_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/pcod_pretemp_int_age_bam_ML.rds"))
+  pcod_yrtemp_int_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/pcod_yrtemp_int_age_bam_ML.rds"))
+  pcod_temp0_int_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/pcod_temp0_int_age_bam_ML.rds"))
   # linear effect of temp and age
   pcod_presurvey_templin_int_age_bam <- readRDS(file = here("./output/model output/ACLIM temps/linear models/pcod_presurvey_templin_int_age_bam.rds"))
 
-  # with temp by age effect -- temp age1 more supported
-  AIC(pcod_temp_int_age_bam_ACLIM_SEBS_ML)
-  AIC(pcod_yrtemp_int_age_bam_ACLIM_SEBS_ML)
-  AIC(pcod_temp1_int_age_bam_ACLIM_SEBS_ML)
-  AIC(pcod_temp0_int_age_bam_ACLIM_SEBS_ML)
+  
+  # with temp by age effect -- summer avg temp more supported
+  AIC(pcod_pretemp_int_age_bam_ML)
+  AIC(pcod_yrtemp_int_age_bam_ML)
+  AIC(pcod_temp0_int_age_bam_ML)
   AIC(pcod_presurvey_templin_int_age_bam)
 
   
-    # yfin 
+   # yfin 
   
-  yfin_temp_int_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/yfin_temp_int_age_bam_ACLIM_SEBS_ML.rds"))
-  yfin_yrtemp_int_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/yfin_yrtemp_int_age_bam_ACLIM_SEBS_ML.rds"))
-  yfin_temp1_int_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/yfin_temp1_int_age_bam_ACLIM_SEBS_ML.rds"))
-  yfin_temp0_int_age_bam_ACLIM_SEBS_ML <- readRDS(file = here("./output/model output/ACLIM temps/ML output/yfin_temp0_int_age_bam_ACLIM_SEBS_ML.rds"))
+	yfin_pretemp_int_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/yfin_pretemp_int_age_bam_ML.rds"))
+  yfin_yrtemp_int_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/yfin_yrtemp_int_age_bam_ML.rds"))
+  yfin_temp0_int_age_bam_ML <- readRDS(file = here("./output/model output/output Feb 2023/yfin_temp0_int_age_bam_ML.rds"))
   # linear effect of temp and age
   yfin_presurvey_templin_int_age_bam <- readRDS(file = here("./output/model output/ACLIM temps/linear models/yfin_presurvey_templin_int_age_bam.rds"))
 
-    # with temp by age effect -- temp age1 more supported
-  AIC(yfin_temp_int_age_bam_ACLIM_SEBS_ML)
-  AIC(yfin_yrtemp_int_age_bam_ACLIM_SEBS_ML)
-  AIC(yfin_temp1_int_age_bam_ACLIM_SEBS_ML)
-  AIC(yfin_temp0_int_age_bam_ACLIM_SEBS_ML)
+  
+  # with temp by age effect -- summer avg temp more supported
+  AIC(yfin_pretemp_int_age_bam_ML)
+  AIC(yfin_yrtemp_int_age_bam_ML)
+  AIC(yfin_temp0_int_age_bam_ML)
   AIC(yfin_presurvey_templin_int_age_bam)
+
+  #########################################################################
+  # Oxygen models
+  #########################################################################
+  
+  
+  # read in all model objects
+  file_list <- list.files(path = paste0(here(), ("/output/model output/ACLIM temps/ML output/Aug 2023")))
+
+  prestring <- paste0(here(), ("/output/model output/ACLIM temps/ML output/Aug 2023/"))
+  
+  mod_names_list <- list()
+  
+  for(i in file_list){
+  	mod_names_list[[i]] <- paste0(prestring, i)
+  }
+  
+  mod_list <- lapply(mod_names_list, readRDS)
+  
+	## compare mods
+	
+  # pcod
+  pcod_oxy_list <- mod_list[grep("pcod", names(mod_list))]
+	
+	lapply(pcod_oxy_list, AICc)
+	
+	# pollock
+	pol_oxy_list <- mod_list[grep("pol", names(mod_list))]
+	
+	lapply(pol_oxy_list, AICc)
+	
+	# yfin sole
+	yfin_oxy_list <- mod_list[grep("yfin", names(mod_list))]
+	
+	lapply(yfin_oxy_list, AICc)
+
+	### compare all temp and oxy mods for each species
+	
+	# pcod
+	names(presurvey_boxy_int_fit)
+	
+	pcod_mod_list <- list(pcod_temp0_int_age_bam_ML, presurvey_boxy_int_fit[[1]])
+	lapply(pcod_mod_list, AICc)
+	# oxygen better
+
+	# pollock
+	names(presurvey_boxy_int_fit)
+
+	pol_mod_list <- list(pol_pretemp_int_age_bam_ML, presurvey_boxy_int_fit[[2]])
+	lapply(pol_mod_list, AICc)
+	# temp better
+
+	# yfin
+	names(presurvey_boxy_int_fit)
+	
+	yfin_mod_list <- list(yfin_pretemp_int_age_bam_ML, presurvey_boxy_int_fit[[3]])
+	lapply(yfin_mod_list, AICc)
+	# temp better
+	
