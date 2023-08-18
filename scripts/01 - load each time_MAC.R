aeb5208a39ec1992edcff274055ce4b6daf9a0ed
jennybigman <jennifersbigman@gmail.com>
2023-08-18 16:36:15 -0700

    add factor coding and update package list

--- scripts/01 - load each time_MAC.R
+++ scripts/01 - load each time_MAC.R
@@ -16,7 +16,10 @@
 	library(sf)
 	library(AICcmodavg)
 	library(sdmTMB)
+	library(sdmTMBextra)
 	library(future)
+	library(visreg)
+	library(ggplot2)
 	
 	# ggsave func
 	 ggsave_func <- function(x,y,w = 10,h = 10){
@@ -34,45 +37,49 @@
 	
 	specimen_dat <- read.csv(file = here("./data/df_list_wrangled_names.csv"))
 
+	# age and cohort as factors and log weight
+	specimen_dat <- specimen_dat %>%
+		mutate(age_f = as.factor(age),
+					 cohort_f = as.factor(cohort),
+					 log_wt = log10(weight)) 
+		
 	# how many NAs per species do I have
 	spec_dat_0 <- specimen_dat %>%
 		dplyr::select(species, age) %>%
 		group_by(species) %>%
 		summarise(NAs = sum(is.na(age))) 
 	
-	# put each species df into a list
-	specimen_dat$haul <- specimen_dat$haul.x
-	specimen_dat$cohort_f <- as.factor(specimen_dat$cohort_f)
+	# have to remove some NAs from pcod df
+	specimen_dat <- specimen_dat %>% 
+		drop_na(c(age, latitude, longitude, 
+							date, month, year, jday, cohort))
 	
-	specimen_dat_list <- list()
- 
-	specimen_dat_list <- specimen_dat %>%
-  	group_split(species) 
+	# change haul col name
+	specimen_dat$haul <- specimen_dat$haul.x
 
 	## code random effect haul and year ##
 	
-	ranef_setup_func <- function(x){
-		
-		year_sort <- sort(unique(x$year))
+		year_sort <- sort(unique(specimen_dat$year))
 		ID <- letters[1:length(year_sort)]
 		
 		year_ID <- data.frame(year_sort, ID) %>%
 			rename(year = year_sort)
 		
-		dat <- merge(x, year_ID, by = "year")
+		specimen_dat <- left_join(specimen_dat, year_ID, by = "year")
 	
-		dat <- dat %>%
+		specimen_dat <- specimen_dat %>%
 			mutate(haul_id = paste(ID, haul, sep = "_"))
 	
-		dat <- dat %>%
+		specimen_dat <- specimen_dat %>%
 		mutate(ID_f = as.factor(ID),
 					 haul_id_f = as.factor(haul_id))
-		
-		
-	}
 	
-	specimen_dat_list <- lapply(specimen_dat_list, ranef_setup_func)
-	
+	# put each species df into a list
+	specimen_dat_list <- list()
+ 
+	specimen_dat_list <- specimen_dat %>%
+  	group_split(species) 
+
 	# add columns
 	
 	weight_metrics_func <- function(x){ 
@@ -105,6 +112,7 @@
   
   hind_var <- ACLIM_weekly_hind %>%
     filter(var %in% vars) %>%
+  	filter(year < 2021) %>%
   	filter(basin == "SEBS")
   
   proj_var <- ACLIM_weekly_fut %>%
@@ -199,32 +207,46 @@
 	
 	specimen_dat <- lapply(specimen_dat, age0_func)
 	
+	# remove 2021 because no output in hindcast and will throw error in sdmTMB()
+	
+	rem_2021 <- function(x){
+		x <- x %>% filter(year != 2021)
+	}
+	
+	specimen_dat <- lapply(specimen_dat, rem_2021)
+	
+	# prep df for sdmTMB()
+	
+	# add XY cols for making mesh
+	xy_func <- function(df){
+	
+		df <- df %>% 
+			dplyr::select(-X) %>%
+			drop_na(longitude, latitude)
+	
+		df <- df %>% sdmTMB::add_utm_columns(., c("longitude", "latitude"))
+
+	}
+	
+	specimen_dat <- lapply(specimen_dat, xy_func)
+		
+	# scale cols
+	
+	std_func <- function(df){
+		
+		  
+   df %>% 
+ 		mutate_at(vars(contains(c("btemp", "boxy", "jday"))), ~ scale(.) %>% as.vector)
+
+	}
+
+	specimen_dat_std <- lapply(specimen_dat, std_func)
+	
+	
 	# separate for species-specific wrangling tasks
-	pcod_dat <- specimen_dat[[1]]
-	pollock_dat <- specimen_dat[[2]]
-	yfinsole_dat <- specimen_dat[[3]]
-	
-	pcod_age_sum <- pcod_dat %>%
-		group_by(age_f) %>%
-		summarize(n())
-		
-		pcod_age_sum %>%
-			head()
-	
-	pollock_age_sum <- pollock_dat %>%
-		group_by(age_f) %>%
-		summarize(n()) 
-	
-		pollock_age_sum	%>%
-			head()
-		
-	yfinsole_age_sum <- yfinsole_dat %>%
-		group_by(age_f) %>%
-		summarize(n()) 
-	
-		yfinsole_age_sum %>%
-			head()
-	
+	pcod_dat <- specimen_dat_std[[1]]
+	pollock_dat <- specimen_dat_std[[2]]
+	yfinsole_dat <- specimen_dat_std[[3]]
 
 	# trim data set by largest age with >= 100 samples
 	pcod_dat <- pcod_dat  %>% filter(between(age, 1, 28))
@@ -233,10 +255,10 @@
 
 	yfinsole_dat <- yfinsole_dat  %>% filter(between(age, 1, 20))
 
-
-	dat_list <- list(pcod_dat, pollock_dat, yfinsole_dat)
-	
-  
- 	pcod_yr_sum <- pcod_dat %>%
-		group_by(year) %>%
-		summarize(n())
+	## into list
+	#dat_list <- list(pcod_dat, pollock_dat, yfinsole_dat)
+	#
+  #
+ 	#pcod_yr_sum <- pcod_dat %>%
+	#	group_by(year) %>%
+	#	summarize(n())
