ddebb5e253adf7786bdad3f0886413a30fed202d
jennybigman <jennifersbigman@gmail.com>
2023-09-02 12:29:22 -0700

    update naming schemes

--- scripts/02b - gams fitted with REML.R
+++ scripts/02b - gams fitted with REML.R
@@ -19,18 +19,14 @@
   				file = here("./output/model output PC/pol_pretemp_age_bam.rds"))
  
   # 2. weight ~ age * temp
-	pol_pretemp_int_age_bam <- bam(log_wt ~ age_f + s(presurvey_mean_temp, by = age_f) + 
-																 	s(julian_day) + te(latitude, longitude) + 
+	pol_pretemp_int_age_bam <- bam(log_wt_std ~ age_f + s(presurvey_btemp, by = age_f) + 
+																 	s(jday) + te(latitude, longitude) + 
 																	s(ID_f, bs = "re") + # haul in year random effect
 								 									s(haul_id_f, bs = "re") + # haul in year random effect
 								 									s(cohort_f, bs = "re"),
 																	data = pollock_dat, nthreads = 8)
-														
-	#saveRDS(pol_pretemp_int_age_bam, 
-  #				file = here("./output/model output/ACLIM temps/pol_pretemp_int_age_bam.rds"))
-  
 	saveRDS(pol_pretemp_int_age_bam, 
-	 				file = here("./output/model output PC/pol_pretemp_int_age_bam.rds"))
+	 				file = here("./output/model output/output Feb 2023/pol_pretemp_int_age_bam.rds"))
  
 	# pcod #
 	
@@ -49,20 +45,38 @@
 	 				file = here("./output/model output PC/pcod_pretemp_age_bam.rds"))
  
   # 2. weight ~ age * temp
-	pcod_pretemp_int_age_bam <- bam(log_wt ~ age_f + s(presurvey_mean_temp, by = age_f) + 
-															 	   s(julian_day) + te(latitude, longitude) + 
+	
+	keepl <- c("3", "4", "5", "6", "7", "8", "9", "10")
+	
+	pcod_dat_trim <- pcod_dat %>%
+		filter(age_f %in% keepl)
+	
+	pcod_dat_trim$age_f <- droplevels(pcod_dat_trim$age_f)
+
+	pcod_pretemp_int_age_bam <- bam(log_wt_std ~ age_f + s(presurvey_btemp, by = age_f) + 
+															 	   s(jday) + te(latitude, longitude) + 
 																   s(ID_f, bs = "re") + # haul in year random effect
 								 								   s(haul_id_f, bs = "re") + # haul in year random effect
 								 								   s(cohort_f, bs = "re"),
-																   data = pcod_dat, nthreads = 8)
+																   data = pcod_dat_trim, nthreads = 8)
 														
-	#saveRDS(pcod_pretemp_int_age_bam, 
-  #				file = here("./output/model output/ACLIM temps/pcod_pretemp_int_age_bam.rds"))
-  
   saveRDS(pcod_pretemp_int_age_bam, 
-  				file = here("./output/model output PC/pcod_pretemp_int_age_bam.rds"))
+  				file = here("./output/model output/output Feb 2023/pcod_pretemp_int_age_bam.rds"))
 
- 	# yfin sole #
+  pcod_pretemp_int_age_bam <- readRDS(file = here("./output/model output/output Feb 2023/pcod_pretemp_int_age_bam.rds"))
+ 	
+  ### with temp0
+  pcod_temp0_int_age_bam <- bam(log_wt_std ~ age_f + s(age0_btemp, by = age_f) + 
+															 	   s(jday) + te(latitude, longitude) + 
+																   s(ID_f, bs = "re") + # haul in year random effect
+								 								   s(haul_id_f, bs = "re") + # haul in year random effect
+								 								   s(cohort_f, bs = "re"),
+																   data = pcod_dat_trim, nthreads = 8)
+														
+  saveRDS(pcod_temp0_int_age_bam, 
+  				file = here("./output/model output/output Feb 2023/pcod_temp0_int_age_bam.rds"))
+
+  # yfin sole #
 	
 	# 1. weight ~ age + temp
 	yfin_pretemp_age_bam <- bam(log_wt ~ age_f + s(presurvey_mean_temp) + 
@@ -79,18 +93,26 @@
   				file = here("./output/model output PC/yfin_pretemp_age_bam.rds"))
 
   # 2. weight ~ age * temp
-	yfin_pretemp_int_age_bam <- bam(log_wt ~ age_f + s(presurvey_mean_temp, by = age_f) + 
-																		s(julian_day) + te(latitude, longitude) + 
+  
+  keepl <- c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
+	
+	yfin_dat_trim <- yfinsole_dat %>%
+		filter(age_f %in% keepl)
+	
+	yfin_dat_trim$age_f <- droplevels(yfin_dat_trim$age_f)
+
+	yfin_pretemp_int_age_bam <- bam(log_wt_std ~ age_f + s(presurvey_btemp, by = age_f) + 
+																		s(jday) + te(latitude, longitude) + 
 																		s(ID_f, bs = "re") + # haul in year random effect
 								 										s(haul_id_f, bs = "re") + # haul in year random effect
 								 										s(cohort_f, bs = "re"),
-																		data = yfinsole_dat, nthreads = 8)
+																		data = yfin_dat_trim, nthreads = 8)
 		
 	#saveRDS(yfin_pretemp_int_age_bam, 
   #				file = here("./output/model output/ACLIM temps/yfin_pretemp_int_age_bam.rds"))
 
   saveRDS(yfin_pretemp_int_age_bam, 
-  				file = here("./output/model output PC/yfin_pretemp_int_age_bam.rds"))
+  				file = here("./output/model output/output Feb 2023/yfin_pretemp_int_age_bam.rds"))
 
   
   #### year prior temps  ####
@@ -98,8 +120,8 @@
 	# pollock #	
   
 	#1. weight ~ age + temp
-	pol_yrtemp_age_bam <- bam(log_wt ~ age_f + s(mean_yr_temp) + 
-															s(julian_day) + te(latitude, longitude) + 
+	pol_yrtemp_age_bam <- bam(log_wt_std ~ age_f + s(presurvey_btemp) + 
+															s(jday) + te(latitude, longitude) + 
 															s(ID_f, bs = "re") + # haul in year random effect
 								 							s(haul_id_f, bs = "re") + # haul in year random effect
 								 							s(cohort_f, bs = "re"),
@@ -111,8 +133,8 @@
 	saveRDS(pol_yrtemp_age_bam, file = here("./output/model output PC/pol_pretemp_age_bam.rds"))
 
   # 2. weight ~ age * temp #### NEXT
-	pol_yrtemp_int_age_bam <- bam(log_wt ~ age_f + s(mean_yr_temp, by = age_f) + 
-																	s(julian_day) + te(latitude, longitude) + 
+	pol_yrtemp_int_age_bam <- bam(log_wt_std ~ age_f + s(presurvey_btemp, by = age_f) + 
+																	s(jday) + te(latitude, longitude) + 
 																	s(ID_f, bs = "re") + # haul in year random effect
 								 									s(haul_id_f, bs = "re") + # haul in year random effect
 								 									s(cohort_f, bs = "re"),
