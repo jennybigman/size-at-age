62cdad592686abf48cb2f6684c41344067048cd5
jennybigman <jennifersbigman@gmail.com>
2023-09-05 11:31:29 -0700

    plot model output from sdmTMB model

--- scripts/04c - viz of sdmTMB mods.R
+++ scripts/04c - viz of sdmTMB mods.R
@@ -1,6 +1,83 @@
 # 04c - visualizations of sdmTMB mods
 
+  presurvey_btemp_int_pol <- readRDS(
+  				file = here("./output/model output/sdmTMB output/presurvey_btemp_int_pol.rds"))
+ 
 	library(visreg)
 	
 	pol_plot_df <- visreg(presurvey_btemp_int_pol, "presurvey_btemp", by = "age_f")
+		
+	ggeffect(presurvey_btemp_int_pol_lin)
+	
+	visreg(presurvey_btemp_int_pol_lin, "presurvey_btemp", by = "age_f", data = pollock_dat)
+	
+	# none of the above works?
+	
+	remotes::install_github("seananderson/ggeffects", ref = "sdmTMB")
+	
+	library(ggeffects)
+
+	pol_plot_df <- ggpredict(presurvey_btemp_int_pol, 
+													 terms = c("presurvey_btemp [all]", "age_f [all]"))
+	
+	cols <- colorRampPalette(c("#000073", "#e5e5f2"))
+	pal <- cols(10)
+	
+	# plot directly from ggpredict
+	p1 <- plot(pol_plot_df, facet = TRUE) + scale_color_manual(values = pal)
+	# only 9 colors built in so need to define colors
+	
+	ggsave(p1, file = here("./output/plots/ggpredict_ex_direct.png"))
+
+	# plot df using ggplot geom_line()
+	p2 <- ggplot(pol_plot_df, aes(x = x, y = predicted)) +
+		geom_ribbon(aes(ymin = conf.low, ymax = conf.high), fill = "#ecedee") +
+		geom_line() +
+		facet_wrap( ~ group) +
+		theme_sleek() +
+		ylab("log scaled weight-at-age") +
+		xlab("Bottom temperature averaged April - June")
+	
+	ggsave(p2, file = here("./output/plots/ggpredict_ex.png"))
+	
+	# plot df using ggplot geom_smooth()
+	p3 <- ggplot(pol_plot_df, aes(x = x, y = predicted)) +
+		#geom_ribbon(aes(ymin = conf.low, ymax = conf.high), fill = "#ecedee") +
+		geom_smooth(method = "loess") +
+		facet_wrap( ~ group) +
+		theme_sleek() +
+		ylab("log scaled weight-at-age") +
+		xlab("Bottom temperature averaged April - June")
+	
+	ggsave(p3, file = here("./output/plots/ggeom_smooth_w_ggpredict_df_ex.png"))
+
+	
+	# try geom_smooth with data not ggpredict df (so not actual model formula or output)
+	p3 <- ggplot(pollock_dat, aes(y = log_wt_std, x = presurvey_btemp)) +
+		geom_smooth(method = "loess") +
+		facet_wrap( ~ age_f) +
+		theme_sleek()
+	
+	ggsave(p3, file = here("./output/plots/ggeom_smooth_noform_ex.png"))
+
+	# try example from https://github.com/pbs-assess/sdmTMB/issues/101
+	
+	ggpredict(presurvey_btemp_int_pol, c("presurvey_btemp", "age_f")) %>% 
+		plot(facet = TRUE) + see::scale_color_flat()
+	#same as above
+	
+	# predict from scratch
+	
+	new_dat <- data.frame(
+		age_f = pollock_dat[, 'age_f', drop = FALSE], # way to keep all values not just unique levels
+		presurvey_btemp = pollock_dat$presurvey_btemp,
+		jday_std = pollock_dat$jday_std
+	)
+	
+	fitted_pol <- ggpredict(presurvey_btemp_int_pol)
+	
+	ggplot(fitted_pol) +
+		geom_line(aes(x = presurvey_btemp, y = est)) +
+		facet_wrap(age_f)
+
 		
