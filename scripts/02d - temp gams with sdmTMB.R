72a55c94d4f0817a7ec1d8e25e86596917bae00e
jennybigman <jennifersbigman@gmail.com>
2023-09-15 05:52:29 -0700

    added spatiotemporal effects

--- scripts/02d - temp gams with sdmTMB.R
+++ scripts/02d - temp gams with sdmTMB.R
@@ -8,7 +8,7 @@
 	yfin_mesh <- make_mesh(yfinsole_dat, xy_cols = c("X", "Y"), cutoff = 20)
 	
 	# matern priors
-	pc <- pc_matern(range_gt = 0.1, sigma_lt = 1)
+	pc <- pc_matern(range_gt = 5, sigma_lt = 1) # range val too small
 	
 	# dataset for pcod has too few samples in age 1 and 2 so remove
   drop_age <- c(1, 2)
@@ -23,12 +23,19 @@
 	####################################################
 
 	#### pollock ####
+	
+	# refine mesh because model with factor-smooth interaction won't converge
+	pol_mesh2 <- make_mesh(pollock_dat, c("X", "Y"), n_knots = 800, type = "kmeans")
+
+	# model with no factor smooth interaction
 	presurvey_btemp_pol <- 
 			sdmTMB(	
- 					log_wt_std ~ age_f + s(presurvey_btemp) + s(jday_std),
+ 					log_wt_std ~ age_f + s(presurvey_btemp) + s(jday_std), # consider linear effect here
 					data = pollock_dat,
 					mesh = pol_mesh,
 					spatial = "on",
+					time = "year",
+					spatiotemporal = "IID",
 					control = sdmTMBcontrol(nlminb_loops = 1, newton_loops = 1))
 	
 	sanity(presurvey_btemp_pol)
@@ -40,20 +47,22 @@
 	saveRDS(presurvey_btemp_pol, 
   				file = here("./output/model output/sdmTMB output/presurvey_btemp_pol.rds"))
  
+	# model with factor-smooth interaction
 	
-	
-	pol_mesh2 <- make_mesh(pollock_dat, c("X", "Y"), n_knots = 800, type = "kmeans")
-
+	# set up priors
+	pc <- pc_matern(range_gt = 6, sigma_lt = 1.7) # set up prior
  		
  	presurvey_btemp_int_pol <- sdmTMB(
 			log_wt_std ~ age_f + s(presurvey_btemp, by = age_f) + s(jday_std),
-			#dispformula = ~ 0, 
 			data = pollock_dat,
-			mesh = pol_mesh2,
+			mesh = pol_mesh,
+			priors = sdmTMBpriors(matern_s = pc),
 			spatial = "on",
+			time = "year",
+			spatiotemporal = "IID",
 			control = sdmTMBcontrol(nlminb_loops = 2, newton_loops = 1))
 
- 	sanity(presurvey_btemp_int_pol) # looks great
+ 	sanity(presurvey_btemp_int_pol) 
  	
  	sims <- simulate(presurvey_btemp_int_pol, nsim = 250)
 	dharma_sims <- sims %>% dharma_residuals(presurvey_btemp_int_pol)
@@ -63,13 +72,15 @@
  
   #### pcod ####
   
+  # model with no interaction
 	presurvey_btemp_pcod <- 
 			sdmTMB(	
  				log_wt_std ~ age_f + s(presurvey_btemp) + s(jday_std),
 				data = pcod_dat_trim,
 				mesh = pcod_mesh_trim,
 				spatial = "on",
-				spatiotemporal = "off",
+				time = "year",
+				spatiotemporal = "IID",
 				control = sdmTMBcontrol(nlminb_loops = 2, newton_loops = 1)) 
 	
 	sanity(presurvey_btemp_pcod)
@@ -81,23 +92,25 @@
 	saveRDS(presurvey_btemp_pcod, 
   				file = here("./output/model output/sdmTMB output/presurvey_btemp_pcod.rds"))
  
+	# model with factor-smooth interaction
+	
 	# adjust mesh
-	#pcod_mesh2 <- make_mesh(pcod_dat_trim, c("X", "Y"), n_knots = 800, type = "kmeans")
+	pcod_mesh_trim2 <- make_mesh(pcod_dat_trim, c("X", "Y"), n_knots = 800, type = "kmeans")
 
- 	presurvey_btemp_int_pcod <- sdmTMB(
+	# set priors
+	pc <- pc_matern(range_gt = 202, sigma_lt = 0.4) # set up prior
+
+ 	presurvey_btemp_int_pcod <- sdmTMB( ##### TROUBLE CONVERGING ############
 			log_wt_std ~ age_f + s(presurvey_btemp, by = age_f) + s(jday_std),
-			dispformula = ~ 0, 
 			data = pcod_dat_trim,
-			mesh = pcod_mesh_trim,
+			mesh = pcod_mesh_trim2,
+			priors = sdmTMBpriors(matern_s = pc),
 			spatial = "on",
-			control = sdmTMBcontrol(nlminb_loops = 3, newton_loops = 3))
+			time = "year",
+			spatiotemporal = "IID",
+			control = sdmTMBcontrol(nlminb_loops = 3))
  	
- 	#presurvey_btemp_int_pcod <- sdmTMB::run_extra_optimization(presurvey_btemp_int_pcod,
- 	#	nlminb_loops = 1L, newton_loops = 1L)
-
  	sanity(presurvey_btemp_int_pcod)
- 	
- 	#### trouble converging ####
  	
  	sims <- simulate(presurvey_btemp_int_pcod, nsim = 250)
 	dharma_sims <- sims %>% dharma_residuals(presurvey_btemp_int_pcod)
@@ -105,15 +118,26 @@
   saveRDS(presurvey_btemp_int_pcod, 
   				file = here("./output/model output/sdmTMB output/presurvey_btemp_int_pcod.rds"))
 
+  presurvey_btemp_int_pcod <- readRDS(
+  				file = here("./output/model output/sdmTMB output/presurvey_btemp_int_pcod.rds"))
+
+
   #### yellowfin sole ####
   
+  # model with no interaction
+  
+  # set up prior	
+  pc <- pc_matern(range_gt = 277, sigma_lt = 0.2) # set up prior
+
 	presurvey_btemp_yfin <- 
 			sdmTMB(	
  				log_wt_std ~ age_f + s(presurvey_btemp) + s(jday_std),
 				data = yfinsole_dat,
 				mesh = yfin_mesh,
+				priors = sdmTMBpriors(matern_s = pc),
 				spatial = "on",
-				spatiotemporal = "off",
+				time = "year",
+				spatiotemporal = "IID",
 				control = sdmTMBcontrol(nlminb_loops = 1, newton_loops = 1)) 
 	
 	sanity(presurvey_btemp_yfin)
@@ -125,17 +149,21 @@
 	saveRDS(presurvey_btemp_yfin, 
   				file = here("./output/model output/sdmTMB output/presurvey_btemp_yfin.rds"))
  
-	# adjust mesh 
-	yfin_mesh2 <- make_mesh(yfinsole_dat, c("X", "Y"), n_knots = 800, type = "kmeans")
+	# model with factor-smooth interaction
+	
+	# add priors
+	pc <- pc_matern(range_gt = 470, sigma_lt = 0.5) # set up prior
 
  	presurvey_btemp_int_yfin <- sdmTMB(
 			log_wt_std ~ age_f + s(presurvey_btemp, by = age_f) + s(jday_std),
-			dispformula = ~ 0, 
 			data = yfinsole_dat,
-			mesh = yfin_mesh2,
+			mesh = yfin_mesh,
 			spatial = "on",
+			priors = sdmTMBpriors(matern_s = pc),
+			time = "year",
+			spatiotemporal = "IID",
 			control = sdmTMBcontrol(nlminb_loops = 3, newton_loops = 2))
-
+ 	
  	sanity(presurvey_btemp_int_yfin)
  	
  	sims <- simulate(presurvey_btemp_int_yfin, nsim = 250)
@@ -144,17 +172,24 @@
   saveRDS(presurvey_btemp_int_yfin, 
   				file = here("./output/model output/sdmTMB output/presurvey_btemp_int_yfin.rds"))
 
+  presurvey_btemp_int_yfin <- readRDS( 
+  				file = here("./output/model output/sdmTMB output/presurvey_btemp_int_yfin.rds"))
+
   ####################################################
 	# yr prior btemp ####
 	####################################################
 
 	#### pollock ####
+
+  # model with no interaction  
 	yrprior_btemp_pol <- 
 			sdmTMB(	
  					log_wt_std ~ age_f + s(yrprior_btemp) + s(jday_std),
 					data = pollock_dat,
 					mesh = pol_mesh,
 					spatial = "on",
+					time = "year",
+					spatiotemporal = "IID",
 					control = sdmTMBcontrol(nlminb_loops = 1, newton_loops = 1))
 	
 	sanity(yrprior_btemp_pol)
@@ -166,13 +201,19 @@
 	saveRDS(yrprior_btemp_pol, 
   				file = here("./output/model output/sdmTMB output/yrprior_btemp_pol.rds"))
  
- 		
+	# model with factor-smooth interaction
+	
+ 	# set up prior
+	pc <- pc_matern(range_gt = 210, sigma_lt = 0.3) # set up prior
+	
  	yrprior_btemp_int_pol <- sdmTMB(
 			log_wt_std ~ age_f + s(yrprior_btemp, by = age_f) + s(jday_std),
-			dispformula = ~ 0, 
 			data = pollock_dat,
 			mesh = pol_mesh,
+			priors = sdmTMBpriors(matern_s = pc),
 			spatial = "on",
+			time = "year",
+			spatiotemporal = "IID",
 			control = sdmTMBcontrol(nlminb_loops = 2, newton_loops = 1))
 
  	sanity(yrprior_btemp_int_pol) # ln_smooth_sigma SE may be large
@@ -184,14 +225,16 @@
   				file = here("./output/model output/sdmTMB output/yrprior_btemp_int_pol.rds"))
  
   #### pcod ####
-  
+ 
+  # model with no interaction 
  	yrprior_btemp_pcod <- 
 			sdmTMB(	
  				log_wt_std ~ age_f + s(yrprior_btemp) + s(jday_std),
 				data = pcod_dat_trim,
 				mesh = pcod_mesh_trim,
 				spatial = "on",
-				spatiotemporal = "off",
+				time = "year",
+				spatiotemporal = "IID",
 				control = sdmTMBcontrol(nlminb_loops = 2, newton_loops = 1)) 
 	
 	sanity(yrprior_btemp_pcod)
@@ -203,25 +246,22 @@
 	saveRDS(yrprior_btemp_pcod, 
   				file = here("./output/model output/sdmTMB output/yrprior_btemp_pcod.rds"))
  
-	# adjust mesh
-	pcod_mesh2 <- make_mesh(pcod_dat_trim, c("X", "Y"), n_knots = 800, type = "kmeans")
-
-	pcod_mesh <- make_mesh(pcod_dat_trim, xy_cols = c("X", "Y"), cutoff = 20)
-
+	# model with factor smooth interaction
+	
+	# set priors
+	pc <- pc_matern(range_gt = 210, sigma_lt = 0.4) # set up prior
+	
  	yrprior_btemp_int_pcod <- sdmTMB(
 			log_wt_std ~ age_f + s(yrprior_btemp, by = age_f) + s(jday_std),
-			dispformula = ~ 0, 
 			data = pcod_dat_trim,
-			mesh = pcod_mesh,
+			mesh = pcod_mesh_trim,
+			priors = sdmTMBpriors(matern_s = pc),
 			spatial = "on",
+			time = "year",
+			spatiotemporal = "IID",
 			control = sdmTMBcontrol(nlminb_loops = 3, newton_loops = 3))
- 	
- 	#presurvey_btemp_int_pcod <- sdmTMB::run_extra_optimization(presurvey_btemp_int_pcod,
- 	#	nlminb_loops = 1L, newton_loops = 1L)
-
+ 
  	sanity(yrprior_btemp_int_pcod) # ln_smooth_sigma SE too large?
- 	
- 	#### trouble converging ####
  	
  	sims <- simulate(yrprior_btemp_int_pcod, nsim = 250)
 	dharma_sims <- sims %>% dharma_residuals(yrprior_btemp_int_pcod)
@@ -229,15 +269,20 @@
   saveRDS(yrprior_btemp_int_pcod, 
   				file = here("./output/model output/sdmTMB output/yrprior_btemp_int_pcod.rds"))
 
+  yrprior_btemp_int_pcod <- readRDS(
+  				file = here("./output/model output/sdmTMB output/yrprior_btemp_int_pcod.rds"))
+
   #### yellowfin sole ####
   
+  # model with no interaction
 	yrprior_btemp_yfin <- 
 			sdmTMB(	
  				log_wt_std ~ age_f + s(yrprior_btemp) + s(jday_std),
 				data = yfinsole_dat,
 				mesh = yfin_mesh,
 				spatial = "on",
-				spatiotemporal = "off",
+				time = "year",
+				spatiotemporal = "IID",
 				control = sdmTMBcontrol(nlminb_loops = 1, newton_loops = 1)) 
 	
 	sanity(yrprior_btemp_yfin)
@@ -249,15 +294,18 @@
 	saveRDS(yrprior_btemp_yfin, 
   				file = here("./output/model output/sdmTMB output/yrprior_btemp_yfin.rds"))
  
-	# adjust mesh 
-	yfin_mesh2 <- make_mesh(yfinsole_dat, c("X", "Y"), n_knots = 800, type = "kmeans")
+	# model with factor smooth interaction
+	
+	# set up prior
+	pc <- pc_matern(range_gt = 210, sigma_lt = 0.4) # set up prior
 
  	yrprior_btemp_int_yfin <- sdmTMB(
 			log_wt_std ~ age_f + s(yrprior_btemp, by = age_f) + s(jday_std),
-			dispformula = ~ 0, 
 			data = yfinsole_dat,
 			mesh = yfin_mesh2,
 			spatial = "on",
+			time = "year",
+			spatiotemporal = "IID",
 			control = sdmTMBcontrol(nlminb_loops = 3,
 															step.min = 0.01, step.max = 1)) ### won't converge
 
@@ -268,6 +316,10 @@
  	
   saveRDS(yrprior_btemp_int_yfin, 
   				file = here("./output/model output/sdmTMB output/presurvey_btemp_int_yfin.rds"))
+  
+  yrprior_btemp_int_yfin <- readRDS(
+  				file = here("./output/model output/sdmTMB output/presurvey_btemp_int_yfin.rds"))
+
 
   ####################################################
 	# age 0 ####
@@ -275,12 +327,15 @@
 
   #### pollock ####
   
+  # model with no interaction
 	age0_btemp_pol <- 
 			sdmTMB(	
  					log_wt_std ~ age_f + s(age0_btemp) + s(jday_std),
 					data = pollock_dat,
 					mesh = pol_mesh,
 					spatial = "on",
+					time = "year",
+					spatiotemporal = "IID",
 					control = sdmTMBcontrol(nlminb_loops = 1, newton_loops = 1))
 	
 	sanity(age0_btemp_pol)
@@ -292,13 +347,19 @@
 	saveRDS(age0_btemp_pol, 
   				file = here("./output/model output/sdmTMB output/age0_btemp_pol.rds"))
  
- 		
+ 	# model with factor-smooth interaction
+	
+	# set up prior
+	pc <- pc_matern(range_gt = 380, sigma_lt = 0.29) # set up prior
+
  	age0_btemp_int_pol <- sdmTMB(
 			log_wt_std ~ age_f + s(age0_btemp, by = age_f) + s(jday_std),
-			dispformula = ~ 0 + as.factor(year), 
 			data = pollock_dat,
 			mesh = pol_mesh,
+			priors = sdmTMBpriors(matern_s = pc),
 			spatial = "on",
+			time = "year",
+			spatiotemporal = "IID",
 			control = sdmTMBcontrol(nlminb_loops = 3, newton_loops = 1))
 
  	sanity(age0_btemp_int_pol) 
@@ -322,13 +383,15 @@
   # adjust mesh due to errors
 	pcod_mesh_trim <- make_mesh(pcod_dat_trim, xy_cols = c("X", "Y"), cutoff = 20)
 
+	# model with no interaction
  	age0_btemp_pcod <- 
 			sdmTMB(	
  				log_wt_std ~ age_f + s(age0_btemp) + s(jday_std),
 				data = pcod_dat_trim,
 				mesh = pcod_mesh_trim,
 				spatial = "on",
-				spatiotemporal = "off",
+				time = "year",
+				spatiotemporal = "IID",
 				control = sdmTMBcontrol(nlminb_loops = 2, newton_loops = 1)) 
 	
 	sanity(age0_btemp_pcod)
@@ -340,6 +403,11 @@
 	saveRDS(age0_btemp_pcod, 
   				file = here("./output/model output/sdmTMB output/age0_btemp_pcod.rds"))
  
+	# model with factor-smooth interaction
+	
+	# set up prior
+	pc <- pc_matern(range_gt = 242, sigma_lt = 0.4) # set up prior
+
 	# adjust mesh
 	pcod_mesh2 <- make_mesh(pcod_dat_trim, c("X", "Y"), n_knots = 800, type = "kmeans")
 
@@ -347,15 +415,14 @@
 
  	age0_btemp_int_pcod <- sdmTMB(
 			log_wt_std ~ age_f + s(age0_btemp, by = age_f) + s(jday_std),
-			dispformula = ~ 0, 
 			data = pcod_dat_trim,
-			mesh = pcod_mesh_trim,
+			mesh = pcod_mesh2,
+			priors = sdmTMBpriors(matern_s = pc),
 			spatial = "on",
-			control = sdmTMBcontrol(nlminb_loops = 3, newton_loops = 3))
+			time = "year",
+			spatiotemporal = "IID",
+			control = sdmTMBcontrol(nlminb_loops = 3))
  	
- 	#presurvey_btemp_int_pcod <- sdmTMB::run_extra_optimization(presurvey_btemp_int_pcod,
- 	#	nlminb_loops = 1L, newton_loops = 1L)
-
  	sanity(age0_btemp_int_pcod) # ln_smooth_sigma SE too large?
  	
  	#### trouble converging ####
@@ -366,15 +433,20 @@
   saveRDS(age0_btemp_int_pcod, 
   				file = here("./output/model output/sdmTMB output/age0_btemp_int_pcod.rds"))
 
+  age0_btemp_int_pcod <- readRDS(
+  				file = here("./output/model output/sdmTMB output/age0_btemp_int_pcod.rds"))
+
   # yellowfin sole ####
   
+  # model with no interaction
 	age0_btemp_yfin <- 
 			sdmTMB(	
  				log_wt_std ~ age_f + s(age0_btemp) + s(jday_std),
 				data = yfinsole_dat,
 				mesh = yfin_mesh,
 				spatial = "on",
-				spatiotemporal = "off",
+				time = "year",
+				spatiotemporal = "IID",
 				control = sdmTMBcontrol(nlminb_loops = 1, newton_loops = 1)) 
 	
 	sanity(age0_btemp_yfin)
@@ -386,16 +458,23 @@
 	saveRDS(age0_btemp_yfin, 
   				file = here("./output/model output/sdmTMB output/age0_btemp_yfin.rds"))
  
+	# model with factor-smooth interaction
+	
+	# set up priors
+	pc <- pc_matern(range_gt = 294, sigma_lt = 0.2) # set up prior
+
 	# adjust mesh 
 	yfin_mesh2 <- make_mesh(yfinsole_dat, c("X", "Y"), n_knots = 800, type = "kmeans")
 
  	age0_btemp_int_yfin <- sdmTMB(
 			log_wt_std ~ age_f + s(age0_btemp, by = age_f) + s(jday_std),
-			dispformula = ~ 0, 
 			data = yfinsole_dat,
 			mesh = yfin_mesh2,
+			priors = sdmTMBpriors(matern_s = pc),
 			spatial = "on",
-			control = sdmTMBcontrol(nlminb_loops = 3,
+			time = "year",
+			spatiotemporal = "IID",
+			control = sdmTMBcontrol(nlminb_loops = 3, newton_loops = 1,
 															step.min = 0.01, step.max = 1)) ### won't converge
 
  	sanity(age0_btemp_int_yfin)
@@ -404,6 +483,9 @@
 	dharma_sims <- sims %>% dharma_residuals(age0_btemp_int_yfin)
  	
   saveRDS(age0_btemp_int_yfin, 
+  				file = here("./output/model output/sdmTMB output/age0_btemp_int_yfin.rds"))
+  
+  age0_btemp_int_yfin <- readRDS( 
   				file = here("./output/model output/sdmTMB output/age0_btemp_int_yfin.rds"))
   
   #### linear ####
