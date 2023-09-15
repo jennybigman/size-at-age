267aebc521c7d2ba8716fc9cfdd2359970ed169c
jennybigman <jennifersbigman@gmail.com>
2023-09-15 05:53:04 -0700

    code to fit all sdmTMB mods in a function

--- scripts/function to fit sdmTMB mods.R
+++ scripts/function to fit sdmTMB mods.R
@@ -1,22 +1,33 @@
 # function to fit sdmTMB mods 
 
 	# put three dfs into one
-
 	dat_all <- bind_rows(pollock_dat, pcod_dat, yfinsole_dat)
 	
+	# function 
+	sdmTMB_no_int_func <- function(sp){
+		
+		new_dat <- dat_all %>% filter(species == sp)
+		
+		mesh <- make_mesh(new_dat, xy_cols = c("X", "Y"), cutoff = 20)
 	
-	sdmTMB_func <- function(sp, var){
-		
-		new_dat <- dat_all <- filter(species == sp)
-		
-		presurvey_btemp <- 
-			sdmTMB(	
- 					log_wt ~ age_f + s(var) + s(jday),
-					data = pollock_dat,
-					mesh = pol_mesh,
+		btemp_mod <- 
+			try(
+				sdmTMB(	
+ 					log_wt ~ age_f + s(presurvey_btemp),
+					data = new_dat,
+					mesh = mesh,
 					spatial = "on",
-					control = sdmTMBcontrol(nlminb_loops = 1, newton_loops = 1,
-																	step.min = 0.01, step.max = 1))
-	
-		
+					time = "year",
+					spatiotemporal = "IID",
+					control = sdmTMBcontrol(nlminb_loops = 1)))
 	}
+	
+	sp_var_names <- expand_grid(
+		sp = unique(dat_all$species),
+		var = names(select(dat_all, contains(c("btemp", "boxy"))))
+	)
+	
+	species_mod <- unique(dat_all$species)
+	
+	no_int_mod_list <- lapply(species_mod, sdmTMB_no_int_func)
+	
