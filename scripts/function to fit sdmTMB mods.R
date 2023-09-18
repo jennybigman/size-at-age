a41a180810cd0877698e9a579bd695184f5ee71b
jennybigman <jennifersbigman@gmail.com>
2023-09-18 15:20:05 -0700

    function to fit linear mods of size at age and temp

--- scripts/function to fit sdmTMB mods.R
+++ scripts/function to fit sdmTMB mods.R
@@ -4,30 +4,79 @@
 	dat_all <- bind_rows(pollock_dat, pcod_dat, yfinsole_dat)
 	
 	# function 
-	sdmTMB_no_int_func <- function(sp){
+	sdmTMB_presurvey_func <- function(sp){
 		
 		new_dat <- dat_all %>% filter(species == sp)
 		
 		mesh <- make_mesh(new_dat, xy_cols = c("X", "Y"), cutoff = 20)
 	
-		btemp_mod <- 
+		pc <- pc_matern(range_gt = 300, sigma_lt = 0.1) # set up prior
+ 		
+		mod <- 
 			try(
 				sdmTMB(	
- 					log_wt ~ age_f + s(presurvey_btemp),
+ 					log_wt ~ age_f + presurvey_btemp,
 					data = new_dat,
 					mesh = mesh,
+					priors = sdmTMBpriors(matern_s = pc),
 					spatial = "on",
 					time = "year",
 					spatiotemporal = "IID",
-					control = sdmTMBcontrol(nlminb_loops = 1)))
+					control = sdmTMBcontrol(nlminb_loops = 3, newton_loops = 1)))
+		
+		mod_int <- 
+			try(
+				sdmTMB(	
+ 					log_wt ~ age_f * presurvey_btemp,
+					data = new_dat,
+					mesh = mesh,
+					spatial = "on",
+					priors = sdmTMBpriors(matern_s = pc),
+					time = "year",
+					spatiotemporal = "IID",
+					control = sdmTMBcontrol(nlminb_loops = 3, newton_loops = 1)))
+		
+		mod_list <- list(mod, mod_int)
+		
 	}
-	
-	sp_var_names <- expand_grid(
-		sp = unique(dat_all$species),
-		var = names(select(dat_all, contains(c("btemp", "boxy"))))
-	)
 	
 	species_mod <- unique(dat_all$species)
 	
-	no_int_mod_list <- lapply(species_mod, sdmTMB_no_int_func)
+	presurvey_mod_list <- lapply(species_mod, sdmTMB_presurvey_func)
 	
+	# extract, merge, and name models
+	presurvey_mod_list1 <- c(presurvey_mod_list[[1]])
+	presurvey_mod_list2 <- c(presurvey_mod_list[[2]])
+	presurvey_mod_list3 <- c(presurvey_mod_list[[3]])
+
+	presurvey_mod_lists <- c(presurvey_mod_list1,
+													 presurvey_mod_list2,
+													 presurvey_mod_list3)
+	
+	sp_name <- c("pol", "pol_int",
+							 "pcod", "pcod_int",
+							 "yfin", "yfin_int")
+	
+	mod_name <- function(x){
+		paste0("lin_presurvey_btemp_", x)
+	}
+	
+	mod_names <- lapply(sp_name, mod_name)
+	
+	names(presurvey_mod_lists) <- mod_names
+	
+	ex_list <- function(x) {
+		list2env(x, globalenv())
+	}
+	
+	sapply(presurvey_mod_lists, ex_list)
+	
+	# check models
+	sanity(lin_presurvey_btemp_pol)
+	sanity(lin_presurvey_btemp_pol_int)
+
+	sanity(lin_presurvey_btemp_pcod)
+	sanity(lin_presurvey_btemp_pcod_int)
+
+	sanity(lin_presurvey_btemp_yfin)
+	sanity(lin_presurvey_btemp_yfin_int)
