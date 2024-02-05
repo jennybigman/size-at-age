dccafd4429169cd6886ce79d7f266fd4b4f7472b
jennybigman <jennifersbigman@gmail.com>
2024-02-05 14:26:36 -0800

    compare models and select those with lowest AIC

--- scripts/03 - model checks and comparison.R
+++ scripts/03 - model checks and comparison.R
@@ -4,11 +4,11 @@
 	
 	# non spatially explicit ####
 	
-	file_list <- list.files(path = paste0(here(), ("/output/model output/sdmTMB output/Jan 2024/")))
+	file_list <- list.files(path = paste0(here(), ("/output/model output/sdmTMB output/Feb 2024 - NN/")))
 
 	file_list <- stringr::str_subset(file_list, '.rds')
 
-  prestring <- paste0(here(), ("/output/model output/sdmTMB output/Jan 2024/"))
+  prestring <- paste0(here(), ("/output/model output/sdmTMB output/Feb 2024 - NN/"))
   
 
   mod_names_list <- list()
@@ -51,171 +51,65 @@
 	
 	sp_mod_lists <- list(pol_mod_list, pcod_mod_list, yfin_mod_list)
 
-	vars <- c("presurvey_btemp", "yrprior_btemp")
+	vars <- dat_all %>%
+		select(contains(c("btemp", "boxy"))) %>%
+		names() 
 
 	df_func <- expand_grid(
 		x = sp_mod_lists,
 		y = vars
 	)
 
-	nspe_AICs <- map2(df_func$x, df_func$y, AIC_func)
-
-	# which temp and which oxygen metric? - yr prior temp & oxygen better than presurvey for all species
-	# oxygen or temp? temp better predictor of changes in size at age for all three species
-	
-	
-	# spatially explicit ####
-
-	file_list <- list.files(path = paste0(here(), ("/output/model output/sdmTMB output/Jan 2024/spatially_exp_temp/")))
-
-  prestring <- paste0(here(), ("/output/model output/sdmTMB output//Jan 2024/spatially_exp_temp/"))
-
-  mod_names_list <- list()
-  
-  for(i in file_list){
-  	mod_names_list[[i]] <- paste0(prestring, i)
-  }
-  
-  mod_list <- lapply(mod_names_list, readRDS)
-  
-  # check sanity
-  
-	sanity_func <- function(x){
-  	 sanity(x)
-  }
-  
-	s <- lapply(mod_list, sanity_func)
-	
-	
-  # separate models by species ####
-  
-	# pollock #
-	pol_mod_list <- mod_list[grep("pol", names(mod_list))]
-	
-	# pcod #
-	pcod_mod_list <- mod_list[grep("pcod", names(mod_list))]
-		
-	# yfin #
-	yfin_mod_list <- mod_list[grep("yfin", names(mod_list))]
-	
-		
-	#### compare models with and without an interaction ####
-	
-	AIC_func <- function(x, y){
-		
-		mods_list <- x[grep(y, names(x))]
-		AIC_list <- lapply(mods_list, AIC)
-	
-	}
-	
-	
-	sp_mod_lists <- list(pol_mod_list, pcod_mod_list, yfin_mod_list)
-	
-	vars <- c("presurvey_btemp", "yrprior_btemp")
-
-	df_func <- expand_grid(
-		x = sp_mod_lists,
-		y = vars
-	)
-
-	spe_AICs <- map2(df_func$x, df_func$y, AIC_func)
-
-	# which temp and which oxygen metric? - yr prior temp & oxygen better than presurvey for all species
-	# oxygen or temp? temp better predictor of changes in size at age for all three species
-	
-	#### survey rep models ####
-	
-	file_list <- list.files(path = paste0(here(), ("/output/model output/sdmTMB output/Jan 2024/roms_survey_rep/")))
-
-  prestring <- paste0(here(), ("/output/model output/sdmTMB output/Jan 2024/roms_survey_rep/"))
-  
-
-  mod_names_list <- list()
-  
-  for(i in file_list){
-  	mod_names_list[[i]] <- paste0(prestring, i)
-  }
-  
-  mod_list <- lapply(mod_names_list, readRDS)
-  
-  # check sanity
-  
-	sanity_func <- function(x){
-  	 sanity(x)
-  }
-  
-	s <- lapply(mod_list, sanity_func)
-	
-	
-  # separate models by species ####
-  
-	# pollock #
-	pol_mod_list <- mod_list[grep("pol", names(mod_list))]
-	
-	# pcod #
-	pcod_mod_list <- mod_list[grep("pcod", names(mod_list))]
-		
-	# yfin #
-	yfin_mod_list <- mod_list[grep("yfin", names(mod_list))]
-	
-		
-	#### compare models with and without an interaction ####
-	
-	AIC_func <- function(x, y){
-		
-		mods_list <- x[grep(y, names(x))]
-		AIC_list <- lapply(mods_list, AIC)
-	
-	}
-	
-	
-	sp_mod_lists <- list(pol_mod_list, pcod_mod_list, yfin_mod_list)
-	
-	vars <- "yr_btemp"
+	AICs <- map2(df_func$x, df_func$y, AIC_func)
 
 
-	df_func <- expand_grid(
-		x = sp_mod_lists,
-		y = vars
-	)
-
-	rsr_AICs <- map2(df_func$x, df_func$y, AIC_func)
-
-# for spatially explicit models
- spe_AICs <-	spe_AICs %>% 
+# df to compare
+ AIC_df <-	AICs %>% 
  	bind_cols() %>%
- 	pivot_longer(cols = contains("temp"), names_to = 'model', values_to = "AIC_sp_exp") 
+ 	pivot_longer(cols = contains(c("temp", "oxy")),
+ 							 names_to = 'model', values_to = "AIC") 
  
- spe_AICs <- spe_AICs[grepl("rds", spe_AICs$model), ]
+ mod_split_func <- function(species){
+ 	
+ 	species_AICs <- AIC_df %>%
+ 		filter(str_detect(model, species))
  
- # non spatially explicit
- nspe_AICs <-	nspe_AICs %>% 
- 	bind_cols() %>%
- 	pivot_longer(cols = contains("temp"), names_to = 'model', values_to = "AIC_nsp_exp")
-
- nspe_AICs <- nspe_AICs[grepl("rds", nspe_AICs$model), ]
-
- # roms survey replicated
- rsr_AICs_bind <-	rsr_AICs %>% 
- 	bind_cols() %>%
- 	pivot_longer(cols = contains("temp"), names_to = 'model', values_to = "AIC_rsr")
-
- rsr_AICs_bind <- rsr_AICs_bind[grepl("rds", rsr_AICs_bind$model), ]
-
- rsr_AICs_yr <- rsr_AICs_bind
- rsr_AICs_yr$model <- gsub("yr_btemp", "yrprior_btemp", rsr_AICs_yr$model)
- rsr_AICs_yr$model <- gsub("_rsr", "", rsr_AICs_yr$model)
-
- rsr_AICs_pre <- rsr_AICs_bind
- rsr_AICs_pre$model <- gsub("yr_btemp", "presurvey_btemp", rsr_AICs_pre$model)
- rsr_AICs_pre$model <- gsub("_rsr", "", rsr_AICs_pre$model)
-
+ 	temp_sp_AIC <- species_AICs %>% 
+ 		filter(str_detect(model,"temp"))
+ 	
+ 	oxy_sp_AIC <- species_AICs %>% 
+ 		filter(str_detect(model, "oxy"))
+ 	
+ 	sp_AIC <- list(temp_sp_AIC, oxy_sp_AIC)
+ 	
+ }
  
- rsr_AICs_mod <- bind_rows(rsr_AICs_yr, rsr_AICs_pre)
-
- # change yr btemp to yr prior just for comparison purposes
+ names <- unique(dat_all$short_name)
  
- AIC_comp <- left_join(nspe_AICs, spe_AICs)
+ sp_AIC_dfs <- lapply(names, mod_split_func) 
+ 
+ pol_comp_temp <- sp_AIC_dfs[[1]][1] %>% bind_rows()
+ 
+ pol_comp_oxy  <- sp_AIC_dfs[[1]][2] %>% bind_rows()
 
- AIC_comp_all <- left_join(AIC_comp, rsr_AICs_mod) 
+ pcod_comp_temp <- sp_AIC_dfs[[2]][1] %>% bind_rows()
+
+ pcod_comp_oxy <- sp_AIC_dfs[[2]][2] %>% bind_rows()
+
+ yfin_comp_temp <- sp_AIC_dfs[[3]][1] %>% bind_rows()
+
+ yfin_comp_oxy <- sp_AIC_dfs[[3]][2] %>% bind_rows()
+ 
+ AIC_list <- list(
+ 	pol_comp_temp, pol_comp_oxy,
+	pcod_comp_temp, pcod_comp_oxy,
+	yfin_comp_temp, yfin_comp_oxy)
+
+ # extract the model with the lowest AIC from each
+ lowest_AIC_func <- function(df){
+ 	
+ 	mod <- df$model[df$AIC == min(df$AIC)]
+ }
+ 
+ lowest_AIC <- lapply(AIC_list, lowest_AIC_func)
  
