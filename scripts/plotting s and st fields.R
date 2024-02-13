8239503eb49eeaf0a877973b6bcf8dce10cb4ed3
jennybigman <jennifersbigman@gmail.com>
2024-02-12 16:44:45 -0800

    add land

--- scripts/plotting s and st fields.R
+++ scripts/plotting s and st fields.R
@@ -1,5 +1,37 @@
-# spatial and spatiotemporal latent fields
+# plot spatial and spatiotemporal latent fields
 
+	# set up spatial polygon
+	
+	# plot temps
+	reg = map_data("world2Hires")
+	reg = subset(reg, region %in% c('USSR', 'USA'))
+	
+	# convert lat longs
+	reg$long = (360 - reg$long)*-1
+	
+	# set map limits
+	lons = c(-181, -160)
+	lats = c(52, 65)
+
+	# quick plotting function from sdmTMB vignette
+	plot_map <- function(dat, column) {
+	 	ggplot(dat, aes(lon, lat, fill = {{ column }})) +
+	 	  geom_raster() +
+			geom_polygon(data = reg, aes(x = long, y = lat, group = group), 
+  		         fill = "darkgrey", color = NA) +
+			coord_fixed(ylim = c(52, 65), xlim = c(-178, -155)) +
+			theme_sleek()
+	}
+	
+	# read in prediction grid of Bering 10k bottom temp
+	grids_all <- fread(file = here("./data/grids_all.csv"))
+
+
+	################################################################################
+	#### spatially explicit - spatially explicit yearly temp
+	################################################################################
+	
+	# load models
 	file_list <- list.files(path = paste0(here(), ("/output/model output/sdmTMB output/Feb 2024 - NN/")))
 	file_list <- stringr::str_subset(file_list, '.rds')
 
@@ -13,39 +45,182 @@
   
   NN_mod_list <- lapply(mod_names_list, readRDS)
   
-  # make a prediction grid
-  pol_survey_grid <- survey_grid_full %>%
-  	rename(presurvey_btemp = temp)
-  
-  pollock_dat <- dat_all %>%
-  	filter(short_name == "pollock")
- 
-  pred_df_func <- function(x){
+  # function to plot spatial field for each model
+  preds_func <- function(mod){
   	
-  	pol_survey_grid <- pol_survey_grid %>%
-  		mutate(age_f = x)
-  	
+  		# grab data
+  		mod_dat <- mod$data
+  		
+  		# which environmental variable?
+  		form <- mod$formula
+	
+  		var <- gsub(".*[()]([^,]+)[,].*", "\\1", form)
+  		
+  		# filter grid by variable and year
+  		
+  		mod_yrs <- sort(unique(mod_dat$year))
+  			
+  		mod_grid <- grid_full %>%
+  			filter(year %in% mod_yrs) %>%
+  			rename("{var}" := temp)
+  		
+  		pred_dat <- expand_grid(
+  			age_f = sort(unique(mod_dat$age_f)),
+  			year = sort(unique(mod_dat$year))
+  		)
+  		
+  		pred_df <- left_join(pred_dat, mod_grid) %>% na.omit
+  		
+  		preds <- predict(mod, pred_df)
+  		
   }
   
-  ages <- sort(unique(pollock_dat$age_f))
-
-  pol_pred_df <- lapply(ages, pred_df_func) %>% bind_rows()
-
-  presurvey_btemp_int_mod_pollock <- NN_mod_list[[8]]
+  preds_all <- purrr::map(NN_mod_list, preds_func)
   
-  preds <- predict(presurvey_btemp_int_mod_pollock, newdata = pol_pred_df)
   
-  plot_map <- function(dat, column) {
-  	ggplot(dat, aes(lat, lon, fill = {{ column }})) +
-  	  geom_raster() +
-  	  coord_fixed()
+  # plot spatial and spatiotemporal fields
+  plot_func <- function(df, mod_name){
+  	
+  	plot_s <-
+  		plot_map(df, omega_s) +
+  		scale_fill_gradient2(limits = c(-0.29, 0.13)) +
+  		ggtitle(paste0("spatial field: ", mod_name, "_sp_exp"))
+   
+  	plot_name_s <- paste0(mod_name, "_sp_field_nn")
+
+		ggsave(plot_s,
+					 file = paste0(here(), "/plots/spatial fields/", plot_name_s, ".png"),
+					 height = 5, width = 5, units = "in")
+  
+  	plot_st <-
+  		plot_map(df, epsilon_st) +
+  		scale_fill_gradient2(limits = c(-0.53, 0.31)) +
+  		facet_wrap(~ year) +
+  		ggtitle(paste0("spatioemporal field: ", mod_name, "_sp_exp"))
+  	
+    plot_name_st <- paste0(mod_name, "_spt_field_nn")
+
+		ggsave(plot_st, 
+					 file = paste0(here(), "/plots/spatiotemporal fields/", plot_name_st, ".png"),
+					 height = 10, width = 15, units = "in")
+
   }
   
-  plot_map(preds, omega_s) +
-  	scale_fill_gradient2() +
-  	ggtitle("Spatial random effects only")
+  mod_names <- names(NN_mod_list)
   
-  plot_map(preds, epsilon_st) +
-  	scale_fill_gradient2() +
-  	facet_wrap(~year) +
-  	ggtitle("Spatiotemporal random effects only")
+  rm_func <- function(name){
+  	
+	mod_names <- name %>% str_remove(".rds")
+	 
+  }
+  
+  mod_names <- purrr::map(mod_names, rm_func)
+
+  
+  fdf <- tibble(
+  	df = preds_all,
+  	mod_name = mod_names
+  )
+  
+	purrr::map2(fdf$df, fdf$mod_name, plot_func)
+	
+	
+	################################################################################
+	#### non spatially explicit - spatially averaged yearly temp
+	################################################################################
+
+	# load models
+	file_list <- list.files(path = paste0(here(), ("/output/model output/sdmTMB output/Jan 2024/")))
+	file_list <- stringr::str_subset(file_list, '.rds')
+
+  prestring <- paste0(here(), ("/output/model output/sdmTMB output/Jan 2024/"))
+  
+  mod_names_list <- list()
+  
+  for(i in file_list){
+  	mod_names_list[[i]] <- paste0(prestring, i)
+  }
+  
+  nspe_mod_list <- lapply(mod_names_list, readRDS)
+  
+  # function to plot spatial field for each model
+  preds_func <- function(mod){
+  	
+  		# grab data
+  		mod_dat <- mod$data
+  		
+  		# which environmental variable?
+  		form <- mod$formula
+	
+  		var <- gsub(".*[()]([^,]+)[,].*", "\\1", form)
+  		
+  		# filter grid by variable and year
+  		
+  		mod_yrs <- sort(unique(mod_dat$year))
+  			
+  		mod_grid <- grid_full %>%
+  			filter(year %in% mod_yrs) %>%
+  			rename("{var}" := temp)
+  		
+  		pred_dat <- expand_grid(
+  			age_f = sort(unique(mod_dat$age_f)),
+  			year = sort(unique(mod_dat$year))
+  		)
+  		
+  		pred_df <- left_join(pred_dat, mod_grid) %>% na.omit
+  		
+  		preds <- predict(mod, pred_df)
+  		
+  }
+  
+  preds_all_nspe <- purrr::map(nspe_mod_list, preds_func)
+  
+  
+  # plot spatial and spatiotemporal fields
+  plot_func_nspe <- function(df, mod_name){
+  	
+  	plot_s <-
+  		plot_map(df, omega_s) +
+  		scale_fill_gradient2(limits = c(-0.29, 0.13)) +
+  		ggtitle(paste0("spatial field: ", mod_name, "_nsp_exp"))
+   
+  	plot_name_s <- paste0(mod_name, "_sp_field_nspe")
+
+		ggsave(plot_s,
+					 file = paste0(here(), "/plots/spatial fields/", plot_name_s, ".png"),
+					 height = 5, width = 5, units = "in")
+  
+  	plot_st <-
+  		plot_map(df, epsilon_st) +
+  		scale_fill_gradient2(limits = c(-0.53, 0.31)) +
+  		facet_wrap(~ year) +
+  		ggtitle(paste0("spatioemporal field: ", mod_name, "_nsp_exp"))
+  	
+    plot_name_st <- paste0(mod_name, "_spt_field_nspe")
+
+		ggsave(plot_st, 
+					 file = paste0(here(), "/plots/spatiotemporal fields/", plot_name_st, ".png"),
+					 height = 10, width = 15, units = "in")
+
+  }
+  
+  mod_names <- names(nspe_mod_list)
+  
+  
+  rm_func <- function(name){
+  	
+		mod_names <- name %>% str_remove(".rds")
+	 
+  }
+  
+  
+  mod_names <- purrr::map(mod_names, rm_func)
+
+  
+  fdf <- tibble(
+  	df = preds_all_nspe,
+  	mod_name = mod_names
+  )
+  
+	purrr::map2(fdf$df, fdf$mod_name, plot_func_nspe)
+	
