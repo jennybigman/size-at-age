553633523ce60661ac16a2eb118382acb4f4b4f4
jennybigman <jennifersbigman@gmail.com>
2024-06-07 11:24:56 -0700

    rename

--- scripts/02d - sdmTMB model function - poly.R
+++ scripts/02d - sdmTMB model function - poly.R
@@ -61,8 +61,14 @@
 	dat_all <- list(pollock_dat, pcod_dat, yfin_dat, atooth_dat) %>% bind_rows()
 
 		
+	
 	# file path to save models
 	file_path_all <- "/output/model output/sdmTMB output/May 2024/poly models/"
+	
+	##################################################################### 
+	# 3rd order poly ####
+	#####################################################################
+
 	
 	#### fit models without interaction ####
 	
@@ -204,6 +210,15 @@
 									matern_st = pc_matern(range_gt = 250, sigma_lt = 2),
 									matern_s = pc_matern(range_gt = 300, sigma_lt = 2))))
 					
+					#time_varying age with AR1 process
+					# years in space-varying 0 + year, match sds match 
+					
+					# notes
+					#time_varying = ~ 1
+					#time_varying_type = "AR1"
+					#intercept for each category being autoregressive, temp effect constant
+					
+				#	bivariate AR1 - field evolving age, group, year
 					
 					s <- sanity(mod_int, gradient_thresh = 0.05)
 	
@@ -234,75 +249,9 @@
 
 	map2(df_func$sp, df_func$y, sdmTMB_int_func)
 
-	#####################################################################
+	##################################################################### 
 	# 2nd order poly ####
 	#####################################################################
-	
-	# sdmTMB function - without CV, polynomial
-	
-	# mesh for each species - best to do this visually for model convergence
-
-	## pollock mesh ####
-	pollock_dat <- dat_all %>% 
-		dplyr::filter(short_name == "pollock")
-
-	pol_mesh <- make_mesh(
-		pollock_dat, c("X", "Y"),
-		fmesher_func = fmesher::fm_mesh_2d_inla,
-			cutoff = 50,
-		#	max.edge = 85#,
-			offset = c(70, 60)
-		)
-	
-	plot(pol_mesh)
-	
-	## pcod mesh ####
-	pcod_dat <- dat_all %>% 
-		dplyr::filter(short_name == "pcod")
-	
-	pcod_mesh <- make_mesh(
-		pcod_dat, c("X", "Y"),
-		fmesher_func = fmesher::fm_mesh_2d_inla,
-			cutoff = 30,
-			#max.edge = 30,
-			offset = c(60, 70)
-		)
-	
-	plot(pcod_mesh)
-	
-	## yellowfin mesh ####
-	yfin_dat <- dat_all %>% 
-		dplyr::filter(short_name == "yfin")
-
-	yfin_mesh <- make_mesh(
-		yfin_dat, c("X", "Y"),
-		fmesher_func = fmesher::fm_mesh_2d_inla,
-			cutoff = 30,
-			#max.edge = 60,
-			offset = c(60, 70)
-		)
-	
-	plot(yfin_mesh)
-	
-	# atooth
-	atooth_dat <- dat_all %>% 
-		filter(short_name == "atooth")
-	
-	atooth_mesh <- make_mesh(
-		atooth_dat, c("X", "Y"),
-		fmesher_func = fmesher::fm_mesh_2d_inla,
-			cutoff = 50,
-		#	max.edge = 85#,
-			offset = c(70, 60)
-		)
-	
-	plot(atooth_mesh)
-
-	dat_all <- list(pollock_dat, pcod_dat, yfin_dat, atooth_dat) %>% bind_rows()
-
-		
-	# file path to save models
-	file_path_all <- "/output/model output/sdmTMB output/May 2024/poly models/"
 	
 	#### fit models without interaction ####
 	
